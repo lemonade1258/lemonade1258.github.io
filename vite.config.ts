fd392dbbb97c83213bbf90dd19b976ea20747d36
lemonade <1779716932@qq.com>
2026-02-10 22:19:03 +0800

    Deploy site

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,11 @@
-import path from 'path';
-import { defineConfig, loadEnv } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, '.', '');
-    return {
-      server: {
-        port: 3000,
-        host: '0.0.0.0',
-      },
-      plugins: [react()],
-      define: {
-        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
-      },
-      resolve: {
-        alias: {
-          '@': path.resolve(__dirname, '.'),
-        }
-      }
-    };
+export default defineConfig({
+  // 对于 lemonade1258.github.io 这种根目录仓库，base 设为 '/'
+  base: "/",
+  build: {
+    outDir: "dist",
+    assetsDir: "assets",
+  },
 });
