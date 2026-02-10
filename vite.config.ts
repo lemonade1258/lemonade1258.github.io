e6bf55e0b35cfe2e321a744dc82fc1463371ab91
lemonade <1779716932@qq.com>
2026-02-10 22:27:09 +0800

    Fix build with flame icon

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  // 对于 lemonade1258.github.io 这种根目录仓库，base 设为 '/'
+  plugins: [react()],
   base: "/",
   build: {
     outDir: "dist",
